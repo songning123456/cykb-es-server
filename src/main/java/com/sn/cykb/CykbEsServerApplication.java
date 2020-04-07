f01d58f08f761e9a0226cb85c08cf2bb945e3799
songning <1457065857@qq.com>
2020-04-07 18:15:35 +0800

    first commit

---
+++ src/main/java/com/sn/cykb/CykbEsServerApplication.java
@@ -0,0 +1,13 @@
+package com.sn.cykb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CykbEsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CykbEsServerApplication.class, args);
+    }
+
+}
