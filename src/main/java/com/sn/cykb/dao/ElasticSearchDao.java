2e70d2c6ab4f941862864896e13b680775edc07f
songning <1457065857@qq.com>
2020-04-14 08:47:45 +0800

    jestClient 初步提交

--- src/main/java/com/sn/cykb/dao/ElasticSearchDao.java
+++ src/main/java/com/sn/cykb/dao/ElasticSearchDao.java
@@ -2,14 +2,20 @@ package com.sn.cykb.dao;
 
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestResult;
-import io.searchbox.core.Bulk;
-import io.searchbox.core.Index;
+import io.searchbox.core.*;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.engine.Engine;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author songning
@@ -23,16 +29,161 @@ public class ElasticSearchDao {
     @Autowired
     private JestClient jestClient;
 
+    /**
+     * 插入单条数据
+     *
+     * @param index
+     * @param type
+     * @param entity
+     * @param <T>
+     * @throws Exception
+     */
     public <T> void save(String index, String type, T entity) throws Exception {
         Index action = new Index.Builder(entity).index(index).type(type).build();
         JestResult jestResult = jestClient.execute(action);
+        if (!jestResult.isSucceeded()) {
+            throw new Exception(jestResult.getErrorMessage());
+        }
     }
 
-    public <T> void saveAll(String index, String type, List<T> list) throws Exception {
+    /**
+     * bulk批量插入
+     *
+     * @param index
+     * @param type
+     * @param list
+     * @param <T>
+     * @throws Exception
+     */
+    public <T> void bulk(String index, String type, List<T> list) throws Exception {
         List<Index> actions = new ArrayList<>();
         assert list != null;
         list.forEach(item -> actions.add(new Index.Builder(item).build()));
         Bulk bulk = new Bulk.Builder().defaultIndex(index).defaultType(type).addAction(actions).build();
         JestResult jestResult = jestClient.execute(bulk);
+        if (!jestResult.isSucceeded()) {
+            throw new Exception(jestResult.getErrorMessage());
+        }
+    }
+
+    /**
+     * select * from :type where :fieldName = :fieldValue limit :from, :size;
+     *
+     * @param index
+     * @param type
+     * @param fieldName
+     * @param fieldValue
+     * @param from
+     * @param size
+     * @return
+     * @throws Exception
+     */
+    public List<SearchResult.Hit<Object, Void>> termQuery(String index, String type, int from, int size, String fieldName, String fieldValue) throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        QueryBuilder queryBuilder = QueryBuilders.termsQuery(fieldName, fieldValue);
+        searchSourceBuilder.query(queryBuilder);
+        searchSourceBuilder.from(from);
+        searchSourceBuilder.size(size);
+        String query = searchSourceBuilder.toString();
+        Search search = new Search.Builder(query).addIndex(index).addType(type).build();
+        SearchResult searchResult = jestClient.execute(search);
+        return searchResult.getHits(Object.class);
+    }
+
+    /**
+     * select * from :type where :fieldName = :fieldValue order by :sortField :sortType limit :from, :size;
+     *
+     * @param index
+     * @param type
+     * @param from
+     * @param size
+     * @param sortField
+     * @param sortType
+     * @param fieldName
+     * @param fieldValue
+     * @return
+     * @throws Exception
+     */
+    public List<SearchResult.Hit<Object, Void>> termQuery(String index, String type, int from, int size, String sortField, boolean sortType, String fieldName, String fieldValue) throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        QueryBuilder queryBuilder = QueryBuilders.termsQuery(fieldName, fieldValue);
+        searchSourceBuilder.query(queryBuilder);
+        searchSourceBuilder.from(from);
+        searchSourceBuilder.size(size);
+        if (sortType) {
+            searchSourceBuilder.sort(sortField, SortOrder.ASC);
+        } else {
+            searchSourceBuilder.sort(sortField, SortOrder.DESC);
+        }
+        String query = searchSourceBuilder.toString();
+        Search search = new Search.Builder(query).addIndex(index).addType(type).build();
+        SearchResult searchResult = jestClient.execute(search);
+        return searchResult.getHits(Object.class);
+    }
+
+    /**
+     * select * from :type where :fieldName in :fieldValues limit :from, :size;
+     *
+     * @param index
+     * @param type
+     * @param fieldName
+     * @param fieldValues
+     * @param from
+     * @param size
+     * @return
+     * @throws Exception
+     */
+    public List<SearchResult.Hit<Object, Void>> termsQuery(String index, String type, int from, int size, String fieldName, String[] fieldValues) throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        QueryBuilder queryBuilder = QueryBuilders.termsQuery(fieldName, fieldValues);
+        searchSourceBuilder.query(queryBuilder);
+        searchSourceBuilder.from(from);
+        searchSourceBuilder.size(size);
+        String query = searchSourceBuilder.toString();
+        Search search = new Search.Builder(query).addIndex(index).addType(type).build();
+        SearchResult searchResult = jestClient.execute(search);
+        return searchResult.getHits(Object.class);
+    }
+
+    /**
+     * select * from :type where :fieldName1 = :fieldValue1 and :fieldName2 = :fieldValue2 and :fieldName3 = :fieldValue3 limit :from, :size;
+     *
+     * @param index
+     * @param type
+     * @param from
+     * @param size
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    public List<SearchResult.Hit<Object, Void>> boolQuery1(String index, String type, int from, int size, Map<String, Object> params) throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        for (Map.Entry<String, Object> item : params.entrySet()) {
+            String fieldName = item.getKey();
+            Object fieldValue = item.getValue();
+            queryBuilder.must(QueryBuilders.termsQuery(fieldName, fieldValue));
+        }
+        searchSourceBuilder.query(queryBuilder);
+        searchSourceBuilder.from(from);
+        searchSourceBuilder.size(size);
+        String query = searchSourceBuilder.toString();
+        Search search = new Search.Builder(query).addIndex(index).addType(type).build();
+        SearchResult searchResult = jestClient.execute(search);
+        return searchResult.getHits(Object.class);
+    }
+
+    /**
+     * select * from type where :id = :id
+     * @param index
+     * @param type
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    public Object findById(String index, String type, String id) throws Exception {
+        Get get = new Get.Builder(index, id).type(type).build();
+        JestResult result = jestClient.execute(get);
+        return result.getSourceAsObject(Object.class);
     }
 }
--- src/test/java/com/sn/cykb/CykbEsServerApplicationTests.java
+++ src/test/java/com/sn/cykb/CykbEsServerApplicationTests.java
@@ -3,15 +3,14 @@ package com.sn.cykb;
 import com.sn.cykb.dao.ElasticSearchDao;
 import com.sn.cykb.entity.Users;
 import com.sn.cykb.util.DateUtil;
+import io.searchbox.core.SearchResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -24,9 +23,26 @@ public class CykbEsServerApplicationTests {
     public void testSave() {
         try {
             List<Users> usersList = this.generateUsersList();
-            elasticSearchDao.saveAll("users_index", "users", usersList);
+            elasticSearchDao.bulk("users_index", "users", usersList);
             Users users = this.generateUsers();
             elasticSearchDao.save("users_index", "users", users);
+            System.out.println("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testQuery() {
+        try {
+//            List<SearchResult.Hit<Object, Void>> result = elasticSearchDao.termQuery("users_index", "users", 0, 30, "updateTime", false, "avatar", "http://");
+//            List<SearchResult.Hit<Object, Void>> result = elasticSearchDao.termsQuery("users_index", "users", 0, 30, "nickName", new String[]{"测试人员0", "测试人员1"});
+//            Map<String, Object> params = new HashMap<>();
+//            params.put("avatar", "http://");
+//            params.put("nickName", "测试人员0");
+//            params.put("updateTime", "2020-04-13 15:52:24");
+//            List<SearchResult.Hit<Object, Void>> result = elasticSearchDao.boolQuery1("users_index", "users", 0, 30, params);
+            Object result = elasticSearchDao.findById("users_index", "users", "ZRmGcnEBj8NokppA4bwD");
             System.out.println("");
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,7 +52,7 @@ public class CykbEsServerApplicationTests {
     private List<Users> generateUsersList() {
         List<Users> usersList = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
-            Users users = Users.builder().id(String.valueOf(Math.random())).uniqueId("1585068219" + i).avatar("http://").gender(1).nickName("测试人员" + i).updateTime(DateUtil.dateToStr(new Date(), "yyyy-MM-dd HH:mm:ss")).build();
+            Users users = Users.builder().uniqueId("1585068219" + i).avatar("http://").gender(1).nickName("测试人员" + i).updateTime(DateUtil.dateToStr(new Date(), "yyyy-MM-dd HH:mm:ss")).build();
             usersList.add(users);
         }
         return usersList;
